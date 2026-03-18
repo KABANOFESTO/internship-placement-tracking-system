41e3503287f6daf551ccf58d49db33c376a56c30
KABANOFESTO <kabanofesto1@gmail.com>
2026-03-18 21:29:30 +0200

    ch:add audit logs

---
+++ backend/auth/permissions.py
@@ -0,0 +1,32 @@
+from rest_framework import permissions
+
+
+class IsAdmin(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == "Admin"
+
+
+class IsSupervisor(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == "Supervisor"
+
+
+class IsStudent(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == "Student"
+
+
+class IsAdminOrSupervisor(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return IsAdmin().has_permission(request, view) or IsSupervisor().has_permission(
+            request, view
+        )
+
+
+class IsAdminSupervisorOrStudent(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return (
+            IsAdmin().has_permission(request, view)
+            or IsSupervisor().has_permission(request, view)
+            or IsStudent().has_permission(request, view)
+        )
