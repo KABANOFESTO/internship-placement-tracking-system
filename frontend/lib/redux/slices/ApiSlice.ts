c9598f4dce327d4f183d19060e7d2baef4b50434
KABANOFESTO <kabanofesto1@gmail.com>
2026-03-20 11:54:40 +0200

    add redux

---
+++ frontend/lib/redux/slices/ApiSlice.ts
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const apiSlice = createApi({
+    reducerPath: "api",
+    baseQuery: fetchBaseQuery({
+        baseUrl: `${process.env.NEXT_PUBLIC_API_URL}/api/`,
+        prepareHeaders: (headers) => {
+            if (typeof window !== "undefined") {
+                const token = localStorage.getItem("access");
+                if (token) {
+                    headers.set("Authorization", `Bearer ${token}`);
+                }
+            }
+            return headers;
+        },
+    }),
+    tagTypes: ["Auth", "AuditLog", "Attendance", "Location", "Task", "TaskUpdate", "Review"],
+    endpoints: () => ({}),
+});
